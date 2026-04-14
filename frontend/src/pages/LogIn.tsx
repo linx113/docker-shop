76e2b020aa3fe2bfb9f78cfe4385c11e12106b6b
vlad113chepurko <vlad113chepurko@gmail.com>
2026-04-14 13:55:37 +0300

    added: form UI/UX

--- frontend/src/pages/LogIn.tsx
+++ frontend/src/pages/LogIn.tsx
@@ -1,5 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { CircleX } from "lucide-react";
+import styles from "./Auth.module.css";
+
 export default function LogIn() {
-  return <form>
-    <h1>Log In</h1>
-  </form>;
+  const navigate = useNavigate();
+
+  return (
+    <form className={styles.form}>
+      <div className={styles.head}>
+        <h1>Log In</h1>
+        <button className={styles.close} type="button">
+          <CircleX onClick={() => navigate("/")} size={25} />
+        </button>
+      </div>
+      <section className={styles.section}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        />
+      </section>
+      <section className={styles.section}>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter password"
+          required
+        />
+      </section>
+      <p>
+        Don't have an account? <Link to="/signup">Sign Up</Link>
+      </p>
+    </form>
+  );
 }
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,7 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./Auth.module.css";
+import { CircleX } from "lucide-react";
+
 export default function SignUp() {
+  const navigate = useNavigate();
+
   return (
-    <form>
-      <h1>Sign Up</h1>
+    <form className={styles.form}>
+      <div className={styles.head}>
+        <h1>Sign Up</h1>
+        <button className={styles.close} type="button">
+          <CircleX onClick={() => navigate("/")} size={25} />
+        </button>
+      </div>
+      <section className={styles.section}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter name"
+          required
+        />
+      </section>
+      <section className={styles.section}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter email"
+          required
+        />
+      </section>
+      <section className={styles.section}>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter password"
+          required
+        />
+      </section>
+      <p>
+        Already have an account? <Link to="/login">Log In</Link>
+      </p>
     </form>
   );
 }
